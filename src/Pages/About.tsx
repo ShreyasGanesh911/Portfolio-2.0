98d033a3521228055586e0c9aadb19e863883a1e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-05 19:53:56 +0530

    Page setup

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
+    <>
+           <section className='page bg-black text-white flex items-center justify-around'>
+      <div className='h-full w-1/3 border displayFlex' style={{height:'60vh'}}>
+      <div className='h-96 w-96 bg-yellow-300'></div>
+     </div>
+      <div className='h-full w-1/3 border' style={{height:'60vh'}}>
+      About
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque eaque omnis molestiae doloribus amet exercitationem quaerat optio unde, quas enim voluptatem dolorum, fuga, magnam labore porro quo sequi voluptatibus vel.</p>
       
-    </div>
+      </div>
+      </section> 
+    </>
   )
 }
