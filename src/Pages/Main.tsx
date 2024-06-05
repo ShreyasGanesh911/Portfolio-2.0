a9e6ac36655d321d2e8754729c43595b540f59e8
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-05 19:53:19 +0530

    Added main page

---
+++ src/Pages/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Landing from './Landing'
+import About from './About'
+import Skills from './Skills'
+import Project from './Project'
+
+export default function Main() {
+  return (
+    <>
+      <Landing/>
+      <About/>
+      <Skills/>
+      <Project/>
+
+    </>
+  )
+}
