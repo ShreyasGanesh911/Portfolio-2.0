f12fda54a1d6853db8040d77d6236f711393a444
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-05 19:52:33 +0530

    Route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes ,Route} from 'react-router-dom';
+import Navbar from './Global/Navbar';
+import Footer from './Global/Footer';
+import ProjectCard from './Components/ProjectCard';
+import Main from './Pages/Main';
 
 function App() {
   return (
     <>
-        hello
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Main/>}></Route>
+          <Route path='/projects/:name' element={<ProjectCard/>}></Route>
+        </Routes>
+        <Footer/>
+        </BrowserRouter>
     </>
   );
 }
