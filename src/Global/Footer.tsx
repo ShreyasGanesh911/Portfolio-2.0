764f34d62a2361e8d040ec192e3077c18df02c7e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-05 19:52:55 +0530

    Added global component

---
+++ src/Global/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-100'>
+      <div className='w-100 bg-violet-400 h-72'></div>
+    </div>
+  )
+}
---
+++ src/Global/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+     <nav className='w-100 sticky top-0 border-b'>
+      <div className='w-100 bg-black h-32 text-white'>
+        navbar
+      </div>
+      </nav> 
+    </>
+  )
+}
