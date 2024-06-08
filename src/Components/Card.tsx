73983b6ea4e4817ea18964668aefa226b60cd212
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-08 23:17:21 +0530

    Card component

---
+++ src/Components/Card.tsx
@@ -0,0 +1,12 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+type Props = {
+    name:string
+}
+export default function Card({name}:Props) {
+  return (
+    <>
+     <motion.div initial={{scale:0,x:200,opacity:0}}  transition={{duration:0.3}} whileInView={{scale:1,opacity:1,x:0}} viewport={{once:true}} whileHover={{y:-5,color:'red'}} className=' w-32 h-12 rouned-lg bg-slate-900 displayFlex mx-1 capitalize font-mono'>{name}</motion.div> 
+    </>
+  )
+}
