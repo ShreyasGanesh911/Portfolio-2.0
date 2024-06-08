cb693a21c12f89734a5d57ec827d5786b71425c8
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-08 23:17:37 +0530

    code cleanup

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -2,18 +2,18 @@ import { motion } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 
 export default function Landing() {
-  const [mouse,setMouse] = useState({x:0,y:0})
-  useEffect(()=>{
-    const myFunc = (e:MouseEvent)=>setMouse({x:e.clientX-25,y:e.clientY-25})
-    window.addEventListener('mousemove',myFunc)
-    return ()=>{
-      window.removeEventListener('mousemove',myFunc)
-    }
-  },[])
+  // const [mouse,setMouse] = useState({x:0,y:0})
+  // useEffect(()=>{
+  //   const myFunc = (e:MouseEvent)=>setMouse({x:e.clientX-25,y:e.clientY-25})
+  //   window.addEventListener('mousemove',myFunc)
+  //   return ()=>{
+  //     window.removeEventListener('mousemove',myFunc)
+  //   }
+  // },[])
   return (
     <>
      <section className='page bg-black text-white flex items-start pt-28 justify-around'>
-      <motion.div animate={{x:mouse.x,y:mouse.y}} transition={{duration:0,delay:0}} style={{height:50,width:50}}  className=' rounded-full fixed top-0 left-0 bg-yellow-300 pointer-events-none'/>
+      {/* <motion.div animate={{x:mouse.x,y:mouse.y}} transition={{duration:0,delay:0}} style={{height:50,width:50}}  className=' rounded-full fixed top-0 left-0 bg-yellow-300 pointer-events-none'/> */}
       <div className='h-full w-1/3 border ' style={{height:'60vh'}}>
         Name div
       </div>
