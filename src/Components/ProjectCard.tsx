1d424ba4199a5d9e89a04aea1b4ded3ae2b9ee7e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-26 19:30:47 +0530

    added css

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import React from 'react'
+import "../Styles/ProjectCard.css"
 import { Link } from 'react-router-dom'
 type Props = {
   projectName : string,
@@ -9,17 +9,20 @@ type Props = {
 export default function ProjectCard({projectName,projectLink,navLink}:Props) {
   return (
     <>
-      <motion.div whileInView={{x:0,scale:1}} style={{backgroundColor:"rgb(32,32,32)"}} initial={{x:100,scale:0.5}} viewport={{once:true}} transition={{stiffness:0}} className='bg-black rounded-xl 2xl:w-3/5 w-2/3 2xl:h-96 h-72 flex my-5'>
-        <div className='w-1/2 displayFlex'>
-          <div className='w-11/12 h-5/6 '>
-            <img className='w-full h-full object-contain' src="https://raw.githubusercontent.com/ShreyasGanesh911/kick-street/master/src/Assets/Readme/desktop-home.png" alt="" />
-          </div>
-        </div>
-        <div className='w-1/2  flex flex-col justify-evenly items-center '>
-          <p className=' w-3/4  text-center displayFlex p-2 text-3xl hover:text-yellow-400'>{projectName}</p>
-          <div className='w-3/4 h-20  flex justify-evenly items-center'>
-            <a href={projectLink} target='_blank' rel='noreferrer' className='border py-1 px-2 rounded hover:text-gray-500'><i className="fa-brands fa-github  mx-1"></i>Project</a>
-            <Link to={`/projects/${navLink}`} target='_blank' rel='noreferrer' className='border py-1 px-2 rounded'><i className="fa-solid fa-arrow-up-right-from-square mx-1"></i>More</Link>
+      <motion.div id='projectCardContainer'  whileInView={{x:0,scale:1}}  initial={{x:100,scale:0.5}} viewport={{once:true}} transition={{stiffness:0}} className=' my-10 w-full  rounded-xl 2xl:w-3/5 xl:w-2/3 2xl:h-96 sm:h-72 sm:flex   items-center'>
+        
+          <motion.div id='projectCardImg' whileHover={{scale:0.98}} className=' bg-yellow-200  displayFlex z-0 ' >
+          <img className='w-full h-full object-contain' src="https://raw.githubusercontent.com/ShreyasGanesh911/kick-street/master/src/Assets/Readme/desktop-home.png" alt="" />
+         
+        </motion.div>
+        <div className={`   sm:w-1/2  h-60 sm:h-full flex  flex-col pt-10`}>
+
+          <p className='  sm:w-3/4 w-full  sm:pl-10 text-center  sm:text-left sm:p-2 2xl:text-5xl text-4xl hover:text-yellow-400'><span className='text-red-400 mx-1'>{'//'}</span>{projectName}</p>
+          {/* <p className='h-1/2'></p> */}
+
+          <div  className='sm:w-3/4 w-full h-32  sm:h-52 flex sm:mt-0 mt-5  flex-col justify-evenly items-center'>
+            <a  href={projectLink} target='_blank' rel='noreferrer' className='border-b border-red-400 hover:border-red-700  w-1/3 displayFlex py-3 px-2 text-lg '><i className="fa-brands fa-github  mx-1"></i>Project</a>
+            <Link to={`/projects/${navLink}`} target='_blank' rel='noreferrer' className='border-b border-red-400 hover:border-red-700 displayFlex w-1/3 py-3 text-lg px-2 '><i className="fa-solid fa-arrow-up-right-from-square mx-1"></i>More</Link>
           </div>
         </div>
       </motion.div>
