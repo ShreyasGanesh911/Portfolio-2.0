54d0597d9e239b797a5ed7ba4ba7aa93bc3f9132
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-28 21:59:27 +0530

    Added project list array

---
+++ src/Constants/projectList.ts
@@ -0,0 +1,7 @@
+export const projectList = [
+    {name:"Kick Street", nav:"kickstreet" , link:"https://github.com/ShreyasGanesh911/kick-street"},
+    {name:"Script Flow", link:"https://github.com/ShreyasGanesh911/kick-street" , nav:"scriptflow"},
+    {name:"FuelCoach", link:"https://github.com/ShreyasGanesh911/kick-street" , nav:"fuelcoach"},
+    {name:"Portfolio", link:"https://github.com/ShreyasGanesh911/kick-street" , nav:"portfolio"},
+    {name:"Flask project", link:"https://github.com/ShreyasGanesh911/kick-street" , nav:""},
+  ]
