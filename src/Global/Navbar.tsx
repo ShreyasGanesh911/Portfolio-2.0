f35340bcd165d2c8212547a38cc2205921c15704
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-29 19:41:15 +0530

    Added toggle switch

--- src/Global/Navbar.tsx
+++ src/Global/Navbar.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
@@ -7,22 +9,56 @@ export default function Navbar() {
     {href:"#projects" , text:"projects"},
     {href:"#contact" , text:"contact"}
   ]
+  const [screen,setScreen] = useState(0)
+  const [show,setShow] = useState(false)
+  const [toggle,setToggle] = useState(false)
+
+  const checkScroll = ()=>{
+    const currentScroll = window.pageYOffset
+    if(currentScroll>1040){
+      console.log("Show now")
+      setShow(true)
+    }
+    if(currentScroll<1040){
+      console.log("not Show now")
+      setShow(false)
+    }
+    setScreen(currentScroll)
+  }
+  useEffect(()=>{
+    window.addEventListener("scroll",checkScroll)
+    return()=> window.removeEventListener("scroll",checkScroll)
+  },[screen,show])
   return (
     <>
-     <nav className=' sticky top-0 bg-black z-20 overflow-hidden' style={{width:'99vw'}}>
-      <div className=' bg-dark flex items-center h-28 text-white justify-between'>
-       <div className="w-16 ml-14">
+     {show && <nav className={` w-full  sticky top-0 bg-black z-20 `} >
+      <div className={` bg-dark flex w-full  items-center h-28 text-white justify-between ${!show?'bg-white':"bg-green-500"}`}>
+       <div className="w-16 sm:ml-14 ml-8">
        <Link to="/"><img src="https://raw.githubusercontent.com/ShreyasGanesh911/Portfolio/master/src/WebLogo.jpeg" className="object-contain" alt="" /></Link>
        </div>
-       <ul className="flex mr-14">
+        <button onClick={()=>setToggle(true)} className="sm:hidden flex absolute top-10 text-4xl right-5"><i className="fa-solid fa-bars"></i></button>
+        {toggle && 
+        <motion.div initial={{scale:0.99,opacity:0,x:100}} animate={{scale:1,opacity:1,x:0}}  transition={{duration:0.2}} 
+        className="bg-dark w-3/5   fixed h-full top-0 right-0 " >
+          <button className=" m-3 mx-3 float-right text-6xl  " onClick={()=>setToggle(false)}>&times;</button>
+          <ul className="flex flex-col  w-full mr-14">
         {list.map((e,i)=>
-        <li className="mx-8 hover:text-red-400 hover:-translate-y-1 transition-all duration-500 hover:border-b hover:border-red-400 ">
+        <li onClick={()=>setToggle(false)} key={i} className="mx-8 hover:text-red-400 hover:-translate-y-1 transition-all duration-500 hover:border-b text-xl my-2 hover:border-red-400 ">
           <a href={e.href} className="capitalize" aria-current="page">{e.text}</a>
         </li>
         )}
        </ul>
+        </motion.div>}
+       <ul className="sm:flex hidden  mr-14">
+        {list.map((e,i)=>
+        <li key={i} className="mx-8 hover:text-red-400 hover:-translate-y-1 transition-all duration-500 hover:border-b hover:border-red-400 ">
+          <a href={e.href} className="capitalize" aria-current="page">{e.text}</a>
+        </li>
+        )}
+       </ul>
+
       </div>
-      </nav> 
+      </nav> }
     </>
   )
 }
