810e69a7c40390e28bfbc274b6eb3ee2c5f53b19
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-31 23:51:02 +0530

    made it responsive

--- src/Global/Footer.tsx
+++ src/Global/Footer.tsx
@@ -1,16 +1,17 @@
 import { motion } from "framer-motion"
 
 //import images from '../Assets/Map.png'
-const contactList = [
-  {name:"GitHub",url:""},
-  {name:"Linkdin",url:""},
-  {name:"shreyas.ganesh911@gmail.com",url:"shreyas.ganesh911@gmail.com"},
-  {name:"Reddit",url:"shreyas.ganesh911@gmail.com"},
-]
+// const contactList = [
+//   {name:"GitHub",url:""},
+//   {name:"Linkdin",url:""},
+//   {name:"shreyas.ganesh911@gmail.com",url:"shreyas.ganesh911@gmail.com"},
+//   {name:"Reddit",url:"shreyas.ganesh911@gmail.com"},
+// ]
 
 export default function Footer() {
   return (
-    <div className='w-full sm:h-96 h-auto sm:flex justify-evenly items-center' style={{width:'99vw',backgroundColor:"rgb(16,16,16)"}}>
+    <div className='w-full sm:h-96 h-auto sm:flex justify-evenly relative items-center sm:pt-0 pt-5' style={{width:'99vw',backgroundColor:"rgb(16,16,16)"}}>
+      <div className="w-full absolute bottom-0 h-1/6 displayFlex text-white font-semibold text-lg mt-5"><p>Crafted with <span className="text-red-500">Passion</span> by Me </p></div>
       <motion.div
         initial={{scale:0.98,opacity:0,x:-50}} whileInView={{scale:1,opacity:1,x:0}} viewport={{once:true}} transition={{duration:0.8}} 
       className="sm:h-5/6 h-72 sm:w-1/4 w-full rounded sm:shadow-lg shadow-slate-700 flex justify-center items-center ">
@@ -22,11 +23,11 @@ export default function Footer() {
       <motion.div 
         initial={{scale:0.98,opacity:0,x:50}} whileInView={{scale:1,opacity:1,x:0}} viewport={{once:true}} transition={{duration:0.8}} 
       className="h-5/6 sm:w-1/4 w-full text-white displayFlex  ">
-        <ul className="flex sm:block sm:w-auto w-full justify-center items-center  ">
-          <li className="my-2 text-xl hover:text-red-400 "><i className="fa-brands fa-github mx-2"></i>Github</li>
-          <li className="my-2 text-xl hover:text-red-400 "><i className="fa-brands fa-linkedin mx-2"></i>Linkdin</li>
-          <li className="my-2 text-xl hover:text-red-400 "><i className="fa-solid fa-envelope mx-2"></i>Mail</li>
-          <li className="my-2 text-xl hover:text-red-400 "><i className="fa-brands fa-discord mx-2"></i>Discord  </li>
+        <ul className=" flex sm:block sm:w-auto w-full  items-center justify-evenly sm:my-0 my-12">
+          <li className="my-2 sm:text-xl text-2xl hover:text-red-400 "><i className="fa-brands fa-github mx-2"></i><span className="sm:inline-block hidden">Github</span></li>
+          <li className="my-2 sm:text-xl text-2xl hover:text-red-400 "><i className="fa-brands fa-linkedin mx-2"></i><span className="sm:inline-block hidden">Linkdin</span></li>
+          <li className="my-2 sm:text-xl text-2xl hover:text-red-400 "><i className="fa-solid fa-envelope mx-2"></i><span className="sm:inline-block hidden">Mail</span></li>
+          <li className="my-2 sm:text-xl text-2xl hover:text-red-400 "><i className="fa-brands fa-discord mx-2"></i><span className="sm:inline-block hidden">Discord</span></li>
           
         </ul>
       </motion.div>
