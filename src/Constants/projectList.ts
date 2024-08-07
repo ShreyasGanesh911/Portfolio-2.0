ff02c65fc65c3829fb0d907e1cd9b2982cfd8c7e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-08-07 20:41:09 +0530

    added description attribute

--- src/Constants/projectList.ts
+++ src/Constants/projectList.ts
@@ -3,25 +3,29 @@ export const projectList = [
       name:"Kick Street", 
       nav:"KickStreet" , 
       link:"https://github.com/ShreyasGanesh911/kick-street",
-      image: "https://raw.githubusercontent.com/ShreyasGanesh911/kick-street/master/src/Assets/Readme/desktop-home.png"
+      image: "https://raw.githubusercontent.com/ShreyasGanesh911/kick-street/master/src/Assets/Readme/desktop-home.png",
+      desc :"Developed a responsive MERN stack e-commerce application that is integrated with Cloudinary for image storage and Razorpay for payment processing."
     },
     {
       name:"Script Flow", 
       link:"https://github.com/ShreyasGanesh911/scriptflow" , 
       nav:"ScriptFlow", 
-      image: "https://raw.githubusercontent.com/ShreyasGanesh911/scriptflow/master/src/Assets/Home.png"
+      image: "https://raw.githubusercontent.com/ShreyasGanesh911/scriptflow/master/src/Assets/Home.png",
+      desc:"Developed an online code editor similar to CodePen, Including a feature that allows users to run and compile programs directly within the platform."
     },
     {
       name:"FuelCoach",
        link:"https://github.com/ShreyasGanesh911/FuelCoach_V2" , 
        nav:"FuelCoach",
-       image:"https://raw.githubusercontent.com/ShreyasGanesh911/FuelCoach_V2/master/public/pictures/HomePage.png"
+       image:"https://raw.githubusercontent.com/ShreyasGanesh911/FuelCoach_V2/master/public/pictures/HomeNew.png",
+       desc:"Developed a user-friendly dietary tracking application utilizing React, designed to monitor daily caloric intake online. The application features a User-friendly and straightforward interface to enhance usability."
       },
     {
       name:"Portfolio",
        link:"https://github.com/ShreyasGanesh911/Portfolio-2.0" ,
        nav:"Portfolio",
-       image:"https://raw.githubusercontent.com/ShreyasGanesh911/Portfolio-2.0/master/src/Assets/Portfolio.png"
+       image:"https://raw.githubusercontent.com/ShreyasGanesh911/Portfolio-2.0/master/src/Assets/Portfolio.png",
+       desc : "Designed and developed a user-friendly, responsive portfolio website to showcase my work and projects."
       },
     // {name:"Flask project", link:"https://github.com/ShreyasGanesh911/kick-street" , nav:"",image:""},
   ]
